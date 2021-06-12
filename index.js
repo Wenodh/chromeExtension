1a91e6d9e2c99c4f3d370b46ed853b46440ba30b
Wenodh <vinodkumara3@gmail.com>
2021-06-13 00:26:16 +0530

    added delete button

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-let myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com'];
+let myLeads = [];
 
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 // console.log(ulEl);
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+if (leadsFromLocalStorage) renderLeads;
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    // console.log(myLeads);
+    inputEl.value = '';
+    localStorage.setItem('myLeads', JSON.stringify(myLeads));
+    renderLeads();
 });
-let listItems = '';
-for (lead in myLeads) {
-    listItems += '<li>' + myLeads[lead] + '</li>';
+
+function renderLeads() {
+    let listItems = '';
+    for (i in myLeads) {
+        listItems += `
+        <li>
+        <a href='https://${myLeads[i]}' target='_blank' rel='noopener noreferrer'>
+           ${myLeads[i]}
+           </a>
+           </li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
-ulEl.innerHTML = listItems;
