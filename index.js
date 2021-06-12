5de222114a0f403b96802f3b79e5ee55efe42065
Wenodh <vinodkumara3@gmail.com>
2021-06-12 14:06:05 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let inputBtn = document.getElementById('input-btn');
+let myLeads = ['www.awesomelead.com', 'www.epiclead.com', 'www.greatlead.com'];
 
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
+// console.log(ulEl);
 inputBtn.addEventListener('click', () => {
-    console.log('Button clicked from addEventListener');
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
 });
+let listItems = '';
+for (lead in myLeads) {
+    listItems += '<li>' + myLeads[lead] + '</li>';
+}
+ulEl.innerHTML = listItems;
